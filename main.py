7c38ad8fd9f86fa2e34380465cd4370bb9e9b6f7
GuardianLeya <74779670+GuardianLeya@users.noreply.github.com>
2023-11-28 23:40:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import telebot
 bot = telebot.TeleBot('6486769450:AAEIju-EuWa6G041VMAihASG59XVfyV_TXM')
 
 
-@bot.message.handler(commands=['start'])
+@bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, 'Привет, я пушистик, а ты?')
 
 
-@bot.message.handler(command=['Polina'])
+@bot.message_handler(command=['Polina'])
 def main(message):
     bot.send_message(message.chat.id, '*я люблю тебя*', parse_mode='Markdown')
 
 
-@bot.message.handler(command=['link'])
+@bot.message_handler(command=['link'])
 def main(message):
     bot.send_message(message.chat.id,
                      'Жмак на [ссылку](https://w.forfun.com/fetch/c4/c493aac67877288476b0fc52d55f55cf.jpeg);)',
