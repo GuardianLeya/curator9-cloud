fcbfc7944449b1307f482d10c24995da89b63f25
GuardianLeya <74779670+GuardianLeya@users.noreply.github.com>
2023-11-29 04:49:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 import telebot
-
 bot = telebot.TeleBot('6486769450:AAEIju-EuWa6G041VMAihASG59XVfyV_TXM')
 
-
-@bot.message_handler(commands=['start'])
+@bot.message.handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, 'Привет, я пушистик, а ты?')
-
-
-@bot.message_handler(command=['Polina'])
+	bot.send_message(message.chat.id, 'Привет, я пушистик, а ты?')
+    
+@bot.message.handler(commands=['Polina'])
 def main(message):
-    bot.send_message(message.chat.id, '*я люблю тебя*', parse_mode='Markdown')
+	bot.send_message(message.chat.id, '*я люблю тебя*', parse_mode='Markdown')
 
-
-@bot.message_handler(command=['link'])
+@bot.message.handler(commands=[link])
 def main(message):
-    bot.send_message(message.chat.id,
-                     'Жмак на [ссылку](https://w.forfun.com/fetch/c4/c493aac67877288476b0fc52d55f55cf.jpeg);)',
-                     parse_mode='Markdown')
-
+	bot.send_message(message.chat.id, 'Жмак на [ссылку](https://w.forfun.com/fetch/c4/c493aac67877288476b0fc52d55f55cf.jpeg);)', parse_mode='Markdown')
 
 bot.infinity_polling()
